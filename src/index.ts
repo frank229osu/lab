d5cbf95ea39c3b8e5f0405fb31578652026145e9
frank229osu <48807400+frank229osu@users.noreply.github.com>
2020-02-20 17:00:30 +0000

    dynamic typing works

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
+document.getElementById('selectedValue').onchange = newTip;
+document.getElementById('total').oninput = runCalcs;
 
-import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+function newTip() {
+    const tipValue = document.getElementById('selectedValue') as HTMLInputElement;
+    document.getElementById('chosenTip').innerHTML = `You've chosen to tip ${tipValue.value}%`;
+    document.getElementById('tipPercent').innerHTML = `Tip percent: ${tipValue.value}%`;
+}
 
+function runCalcs() {
+    const inputValue = document.getElementById('total') as HTMLInputElement;
+    const tipValue = document.getElementById('selectedValue') as HTMLInputElement;
+    const tipAmount: number = parseInt(inputValue.value) * (parseInt(tipValue.value) / 100);
+    const totalPaid: number = parseInt(inputValue.value) + tipAmount;
+    document.getElementById('billAmount').innerHTML = `Bill Amount: ${inputValue.value}%`;
+
+    document.getElementById('tipAmount').innerHTML = `Amount of tip: $${tipAmount}`;
+    document.getElementById('totalPaid').innerHTML = `Total to be paid: $${totalPaid}`;
+}
