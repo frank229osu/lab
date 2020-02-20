7288bcc08881c0212e69f7217e554a35239e2d4b
frank229osu <48807400+frank229osu@users.noreply.github.com>
2020-02-20 21:02:04 +0000

    working on tip persistence

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,68 @@
-document.getElementById('selectedValue').onchange = newTip;
-document.getElementById('total').oninput = runCalcs;
+document.getElementById('selectedValue').onchange = onTipChange;
+document.getElementById('total').oninput = onBillChange;
 let tipValue = document.getElementById('selectedValue') as HTMLInputElement;
 
-function newTip() {
-    tipValue = document.getElementById('selectedValue') as HTMLInputElement;
-    document.getElementById('chosenTip').innerHTML = `You've chosen to tip ${tipValue.value}%`;
-    document.getElementById('tipPercent').innerHTML = `Tip percent: ${tipValue.value}%`;
-    saveIt();
+const storedTip = localStorage.getItem('yourTipChoice');
+
+if (storedTip) {
+    document.getElementById('selectedValue').setAttribute(tipValue.value, storedTip);
 }
 
-function runCalcs() {
+function onBillChange() {
     const inputValue = document.getElementById('total') as HTMLInputElement;
-    const tipValue = document.getElementById('selectedValue') as HTMLInputElement;
-    const tipAmount: number = parseInt(inputValue.value) * (parseInt(tipValue.value) / 100);
-    const totalPaid: number = parseInt(inputValue.value) + tipAmount;
-    document.getElementById('billAmount').innerHTML = `Bill Amount: ${inputValue.value}%`;
+    const tipPercent = document.getElementById('selectedValue') as HTMLInputElement;
 
-    document.getElementById('tipAmount').innerHTML = `Amount of tip: $${tipAmount}`;
-    document.getElementById('totalPaid').innerHTML = `Total to be paid: $${totalPaid}`;
+    const inputValueAsNumber: number = parseInt(inputValue.value);
+    const tipPercentAsNumber: number = parseInt(tipPercent.value) / 100;
+
+    if (tipPercentAsNumber > 0) {
+        updateBillAmount(inputValueAsNumber);
+        updateTipPercent(tipPercentAsNumber);
+        updateTipAmount(inputValueAsNumber, tipPercentAsNumber);
+        updateTotalPaid(inputValueAsNumber, tipPercentAsNumber);
+    }
+}
+
+function onTipChange() {
+    const inputValue = document.getElementById('total') as HTMLInputElement;
+    const tipPercent = document.getElementById('selectedValue') as HTMLInputElement;
+
+    const inputValueAsNumber: number = parseInt(inputValue.value);
+    const tipPercentAsNumber: number = parseInt(tipPercent.value) / 100;
+
+    updateChosenTip(tipPercentAsNumber);
+
+    if (inputValueAsNumber > 0) {
+
+        updateBillAmount(inputValueAsNumber);
+        updateTipPercent(tipPercentAsNumber);
+        updateTipAmount(inputValueAsNumber, tipPercentAsNumber);
+        updateTotalPaid(inputValueAsNumber, tipPercentAsNumber);
+        saveIt();
+    }
+}
+
+function updateChosenTip(tipPercentAsNumber: number) {
+    document.getElementById('chosenTip').innerHTML = `You chose to tip: ${tipPercentAsNumber * 100}%`;
+}
+
+function updateBillAmount(inputValueAsNumber: number) {
+    document.getElementById('billAmount').innerHTML = `Bill Amount: $${inputValueAsNumber}`;
+}
+
+function updateTipPercent(tipPercentAsNumber: number) {
+    document.getElementById('tipPercent').innerHTML = `Tip percent: ${tipPercentAsNumber}%`;
+}
+
+function updateTipAmount(inputValueAsNumber: number, tipPercentAsNumber: number) {
+    document.getElementById('tipAmount').innerHTML = `Amount of tip: $${tipPercentAsNumber * inputValueAsNumber}`;
+}
+
+function updateTotalPaid(inputValueAsNumber: number, tipPercentAsNumber: number) {
+    const totalValue = inputValueAsNumber + (inputValueAsNumber * tipPercentAsNumber);
+    document.getElementById('totalPaid').innerHTML = `Total to be paid: $${totalValue}`;
 }
 
 function saveIt() {
-    localStorage.setItem('YourTip', tipValue.value);
+    localStorage.setItem('yourTipChoice', tipValue.value);
 }
